32e4e452a8aecdc9e5c02fb1cc123d3dc7b8e282
sawmon71293 <sawmon71293@gmail.com>
2024-03-04 17:27:40 +0700

    indent

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <script setup>
  
 </script>
+
 <template>
+
   <div>
+
     <h2>Home</h2>
+
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
       commodi!
     </p>
+
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis,
       corporis?
@@ -18,7 +23,9 @@
     
 
   </div>
+
 </template>
+
 <style
   src="@/assets/common.sass"
   lang="sass"
